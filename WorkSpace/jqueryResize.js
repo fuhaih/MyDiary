9d42e9d41ab34219a7aec40c0cbb42505a10942d
fuhaih <1154590036@qq.com>
2017-08-23 09:53:45 +0800

    2017-8-23

---
+++ WorkSpace/jqueryResize.js
@@ -0,0 +1,108 @@
+﻿/**
+*div的resize方法
+*用法$("").reSize();
+*/
+
+//错误
+(function ($, h, c) {
+    //$=jQuery
+    //h=this=window
+    //c=undefined
+
+    var a = $([]),//创建jquery数组
+        e = $.reSize = $.extend($.reSize, {}),//$.reSize={},e=$.reSize
+        i,
+        k = "setTimeout",
+        j = "reSize",
+        d = j + "-special-event",
+        b = "delay",
+        f = "throttleWindow",
+        reSize = $.fn.reSize = function (a, c) {
+            return arguments.length > 0 ? this.on(j, null, a, c) : this.trigger(j)
+        }
+        //resize = $.fn.resize = function (a, c) {
+        //    return arguments.length > 0 ? this.on($.b, null, a, c) : this.trigger($.b)
+        //}
+    e[b] = 250;
+    e[f] = true;
+    //$.event.special自定义事件
+    $.event.special[j] = {
+        /** 
+         * 初始化事件处理器 - this指向元素 
+         * @param 附加的数据 
+         * @param 事件类型命名空间 
+         * @param 回调函数 
+         */
+        setup: function () {
+            if (!e[f] && this[k]) {
+                return false;
+            }
+            var l = $(this);
+            a = a.add(l);
+            //jquery 的data模块
+            // 设置,读取自定义数据
+            //data: function( elem, name, data ) {}
+            $.data(this, d, {
+                w: l.width(),
+                h: l.height()
+            });
+            if (a.length === 1) {
+                g();
+            }
+        },
+        /** 
+         * 卸载事件处理器 - this指向元素 
+         * @param 事件类型命名空间 
+         */
+        teardown: function () {
+            if (!e[f] && this[k]) {
+                return false;
+            }
+            var l = $(this);
+            a = a.not(l);
+            l.removeData(d);
+            if (!a.length) {
+                clearTimeout(i);
+            }
+        },
+        add: function (l) {
+            if (!e[f] && this[k]) {
+                return false;
+            }
+            var n;
+            function m(s, o, p) {
+                var q = $(this),
+                    r = $.data(this, d);
+                r.w = o !== c ? o : q.width();
+                r.h = p !== c ? p : q.height();
+                n.apply(this, arguments);
+            }
+            if ($.isFunction(l)) {
+                n = l;
+                return m;
+            } else {
+                n = l.handler;
+                l.handler = m;
+            }
+        }
+    };
+    function g() {
+        /*
+        setTimeout()方法不断执行 e[b]=250毫秒
+        */
+        i = h[k](function () {
+            a.each(function () {
+                var n = $(this),
+                    m = n.width(),
+                    l = n.height(),
+                    o = $.data(this, d);
+                if (m !== o.w || l !== o.h) {
+                    //当元素的长宽变动的时候，用trigger来触发事件
+                    n.trigger(j, [o.w = m, o.h = l]);
+                }
+            });
+            g();
+        },
+            e[b]);
+    }
+})(jQuery, this);
---
+++ WorkSpace/jsTool.js
@@ -0,0 +1,253 @@
+﻿/*
+*json格式的datetime转为js格式的datetime
+*/
+function timeJson2Date(time) {
+    if (time != null) {
+        var date = new Date(parseInt(time.replace("/Date(", "").replace(")/", ""), 10));
+        return date;
+    }
+    return null;
+}
+
+// 对Date的扩展，将 Date 转化为指定格式的String
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
+// 例子： 
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null)
+        return unescape(r[2]);
+    return null;
+}
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+	//number为分组里的内容
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+    });
+};
+
+var getRandomColor = function () {
+    return '#' + (function (color) {
+        //这个写法比较有意思,Math.floor(Math.random()*16);返回的是一个小于或等于16的数.然后作为0123456789abcdef的下标,这样每次就会得到一个这个字符串当中的一个字符
+        return (color += '0123456789abcdef'[Math.floor(Math.random() * 16)])
+            //然后判断这个新字符串的长度是否到6,因为16进制的颜色是由6个字符组成的,如果到6了,就返回这6个字符拼成的字符串,如果没有就执行arguments.callee(color)也就是函数本身.
+            && (color.length == 6) ? color : arguments.callee(color); //将''字符串传给color
+    })('');
+}
+
+/*
+* 把字符串转换为适用于.net的unicode编码
+*/
+String.prototype.ToUnicodeBytes = function () {
+    var ch, re = [];
+    for (var i = 0; i < this.length; i++) {
+        ch = this.charCodeAt(i);  // get char   
+        if (ch > 255)// set up "stack"  
+        {
+            re.push(ch & 0xFF);  // push byte to stack  
+            ch = ch >> 8;          // shift value down by 1 byte  
+            re.push(ch & 0xFF);
+        }
+        else {
+            re.push(ch & 0xFF);
+            re.push(0);
+        }
+    }
+    // return an array of bytes  
+    return re;
+}
+
+/*
+* 把unicode编码转换为字符串
+*/
+UnicodeToString = function (array) {
+    if (array.length & 1 == 1) return;
+    var charCode = [];
+    for (var i = 0; i < array.length; i = i + 2) {
+        var code = array[i + 1];
+        code = code << 8;
+        code = code | array[i];
+        var str = String.fromCharCode(code)
+        charCode.push(str);
+    }
+    var res = charCode.join('');
+    return res;
+}
+
+/** 
+*@param {string} url 完整的URL地址 
+*@returns {object} 自定义的对象 
+*@description 用法示例：var myURL = parseURL('http://abc.com:8080/dir/index.html?id=255&m=hello#top'); 
+
+myURL.file='index.html' 
+
+myURL.hash= 'top' 
+
+myURL.host= 'abc.com:8081' 
+
+myURL.hostname= 'abc.com' 
+
+myURL.query= '?id=255&m=hello' 
+
+myURL.params= Object = { id: 255, m: hello } 
+
+myURL.path= '/dir/index.html' 
+
+myURL.segments= Array = ['dir', 'index.html'] 
+
+myURL.port= '8081' 
+
+myURL.protocol= 'http' 
+
+myURL.source= 'http://abc.com:8080/dir/index.html?id=255&m=hello#top' 
+
+*/
+function parseURL(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    return {
+        source: url,
+        protocol: a.protocol.replace(':', ''),
+        hostname: a.hostname,
+        host: a.host,
+        port: a.port,
+        query: a.search,
+        params: (function () {
+            var ret = {},
+                seg = a.search.replace(/^\?/, '').split('&'),
+                len = seg.length, i = 0, s;
+            for (; i < len; i++) {
+                if (!seg[i]) { continue; }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        })(),
+        file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+        hash: a.hash.replace('#', ''),
+        path: a.pathname.replace(/^([^\/])/, '/$1'),
+        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+        segments: a.pathname.replace(/^\//, '').split('/')
+    };
+}
+
+/*
+*iframe自适应子页面内容
+*@down：iframe元素ID
+*/
+function dyniframesize(down) {
+    var pTar = null;
+    if (document.getElementById) {
+        pTar = document.getElementById(down);
+    }
+    else {
+        eval('pTar = ' + down + ';');
+    }
+    if (pTar && !window.opera) {
+        //begin resizing iframe 
+        pTar.style.display = "block"
+        if (pTar.contentDocument && pTar.contentDocument.body.offsetHeight) {
+            //ns6 syntax 
+            pTar.height = pTar.contentDocument.body.scrollHeight + 20;
+            //pTar.height = pTar.contentDocument.documentElement.scrollHeight + 20;
+            //pTar.width = pTar.contentDocument.body.scrollWidth + 20;
+        }
+        else if (pTar.Document && pTar.Document.body.scrollHeight) {
+            //ie5+ syntax 
+            pTar.height = pTar.Document.body.scrollHeight;
+            pTar.width = pTar.Document.body.scrollWidth;
+        }
+    }
+}
+
+
+function getBrowser()
+{
+    var userAgent = navigator.userAgent,
+    ua = userAgent.toLowerCase(), 
+    rMsie = /(msie\s|trident.*rv:)([\w.]+)/,
+    rFirefox = /(firefox)\/([\w.]+)/,
+    rOpera = /(opera).+version\/([\w.]+)/,
+    rChrome = /(chrome)\/([\w.]+)/,
+    rSafari = /version\/([\w.]+).*(safari)/;
+    var match = rMsie.exec(ua);
+    if (match != null) {
+        return { browser: "IE", version: match[2] || "0" };
+    }
+    var match = rFirefox.exec(ua);
+    if (match != null) {
+        return { browser: match[1] || "", version: match[2] || "0" };
+    }
+    var match = rOpera.exec(ua);
+    if (match != null) {
+        return { browser: match[1] || "", version: match[2] || "0" };
+    }
+    var match = rChrome.exec(ua);
+    if (match != null) {
+        return { browser: match[1] || "", version: match[2] || "0" };
+    }
+    var match = rSafari.exec(ua);
+    if (match != null) {
+        return { browser: match[2] || "", version: match[1] || "0" };
+    }
+    if (match != null) {
+        return { browser: "", version: "0" };
+    }
+}
+
+//给不支持forEach的浏览器数组添加forEach函数
+if (!Array.prototype.forEach) {
+
+    Array.prototype.forEach = function forEach(callback, thisArg) {
+
+        var T, k;
+
+        if (this == null) {
+            throw new TypeError("this is null or not defined");
+        }
+        var O = Object(this);
+        var len = O.length >>> 0;
+        if (typeof callback !== "function") {
+            throw new TypeError(callback + " is not a function");
+        }
+        if (arguments.length > 1) {
+            T = thisArg;
+        }
+        k = 0;
+
+        while (k < len) {
+
+            var kValue;
+            if (k in O) {
+
+                kValue = O[k];
+                callback.call(T, kValue, k, O);
+            }
+            k++;
+        }
+    };
+}
